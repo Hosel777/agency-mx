8fa86bf7a74241ed2876c30aa1d18837386bcc18
Hosel777 <businesslatamarg@gmail.com>
2026-06-06 04:08:12 -0300

    Fix CORS: add Access-Control-Allow-Origin for SocialPulse

--- api/external-request.js
+++ api/external-request.js
@@ -1,4 +1,27 @@
+const ALLOWED_ORIGINS = [
+  'https://marketing-app-jola.vercel.app',
+  'https://agency-mx.vercel.app',
+  'http://localhost:5173',
+  'http://localhost:3000'
+]
+
+function setCorsHeaders(res, origin) {
+  if (ALLOWED_ORIGINS.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin)
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-api-key')
+  res.setHeader('Access-Control-Max-Age', '86400')
+}
+
 export default async function handler(req, res) {
+  const origin = req.headers.origin
+  setCorsHeaders(res, origin)
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end()
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' })
   }
